f8084c16334bf34b6c02b5e52fa161c784fd57cc
artem.ivanov <artem.ivanov@dsr-corporation.com>
2023-10-10 07:56:18 +0300

    feat: Refactored generate genesis script
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-corporation.com>

--- smart_contracts/scripts/genesis/generate.ts
+++ smart_contracts/scripts/genesis/generate.ts
@@ -1,17 +1,24 @@
 import { writeJson } from '../../utils'
 import { outFile } from './config'
-import { accountControl, credentialDefinitions, dids, didValidator, roles, schemas, validators } from './contracts'
+import {
+  accountControl,
+  credentialDefinitionRegistry,
+  didRegistry,
+  didValidator,
+  roleControl,
+  schemaRegistry,
+  validatorControl,
+} from './contracts'
 
 function main() {
   const contracts = {
     ...accountControl(),
-    ...roles(),
-    ...validators(),
-    ...didRegex(),
+    ...roleControl(),
+    ...validatorControl(),
     ...didValidator(),
-    ...dids(),
-    ...schemas(),
-    ...credentialDefinitions(),
+    ...didRegistry(),
+    ...schemaRegistry(),
+    ...credentialDefinitionRegistry(),
   }
   writeJson(contracts, outFile)
 }
